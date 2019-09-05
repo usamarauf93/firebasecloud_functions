783e00ee425951623bc410c12c67f578d1d60c19
Raheel <ibtihaaj.khurram@abfatechnologies.com>
2019-09-05 17:03:56 +0500

    changes for show lat lng of washers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,33 +3,28 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp()
-export const getLahoreWeather = functions.https.onRequest((request, response) => {
-    admin.firestore().doc('weather/lahore').get()
-    .then(snapshot=>{
-        const data = snapshot.data()
-        response.send(data)
-    })
-    .catch(error =>{
-        console.log(error)
-        response.status(500).send(error)
-    })
-})
-// exports.onOrderCreate = functions.database.ref('/AllOrders')
-//     .onCreate((snapshot, context) => {   
-//       console.log('New Order Capture ', context.params.pushId);
-//     });
+
   exports.onOrderCreate = functions.database.ref('/AllOrders/{orderId}')
     .onCreate((snapshot, context) => {
      
-      console.log('this is new order created: ', context.params)
-      console.log('data: ', snapshot.val())
-      return snapshot.ref.once('child_added')
+      console.log('this is new order created key: ', context.params)
+        const orderData = snapshot.val()
+        console.log('data: ', orderData.lat+'  '+orderData.lng)
+
+        let output = admin.database().ref('/WasherTeams').once('value', (snapshot2) => {
+        
+
+              
+              snapshot2.forEach((child) => {
+                  console.log(child.key, child.val())
+                  let data = child.val()
+                  console.log('the latlng of washers'+data.teamLat+'  '+ data.teamLng)
+                   
+                 
+                  console.log("this is foreach")
+                })
+              
+          })
+  
+      return output
     })
-
-    exports.showOrder = functions.https.onRequest((req, res) => {
-
-      return admin.database().ref('/AllOrders/').once('value', (snapshot) => {
-          const data = snapshot.val();
-          res.send( data.child );
-       });
-  });
